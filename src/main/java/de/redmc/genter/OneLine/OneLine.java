f5b3ddb3dd17693c0cbad4c6320eb0b3fca12c28
Dustin <genterhd@gmail.com>
2018-09-13 00:36:41 +0200

    [+] plugin.yml [+] getInstance()

--- src/main/java/de/redmc/genter/OneLine/OneLine.java
+++ src/main/java/de/redmc/genter/OneLine/OneLine.java
@@ -1,12 +1,19 @@
 package de.redmc.genter.OneLine;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OneLine extends JavaPlugin {
 
+    public static OneLine getInstance() {
+        return getPlugin(OneLine.class);
+    }
+
+    public String prefix = "§8";
+
     @Override
     public void onEnable() {
-
+        Bukkit.getConsoleSender().sendMessage("");
     }
 
     @Override

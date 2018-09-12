e0034361a7ad21d58563632735cf5a2d3739a4db
Dustin <genterhd@gmail.com>
2018-09-13 00:26:40 +0200

    [+] Plugin create

---
+++ src/main/java/de/redmc/genter/OneLine/OneLine.java
@@ -0,0 +1,16 @@
+package de.redmc.genter.OneLine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OneLine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
